eb8668dbde02211b53e29c261eecd61842621746
kchokshi <kchokshi@users.noreply.github.com>
2015-04-20 23:18:32 -0400

    Update Transaction.cs

--- abc-bank/Transaction.cs
+++ abc-bank/Transaction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +12,15 @@ namespace abc_bank
 
         private DateTime transactionDate;
 
-        public Transaction(double amount) 
+        public Transaction(double amount)
         {
             this.amount = amount;
             this.transactionDate = DateProvider.getInstance().Now();
+        }
+
+        public DateTime GetTransactionDate()
+        {
+            return this.transactionDate;
         }
     }
 }
